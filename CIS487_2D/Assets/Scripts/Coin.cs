a31ca3bb063c93a5006e23da293e517b45e7a1a2
AndrewRodman <rodman@umich.edu>
2016-10-22 20:27:21 -0400

    Added some more features and sounds 10/22
    - Added a Splash and Menu Scene (Using the background on Menu Scene as
    Example)
    - Added a restart level button and Go to menu button on win/crash window
    - Player can now crash if they touch the bottom edge collider of the
    canvas
    - Coins and coin animations added and can be collected
    - Fire animation when gas is pressed
    - Can now run out of gas
    - Sound FX added for Gas and Coin pickup
    - Music added for Menu Scene and Scene 1

---
+++ CIS487_2D/Assets/Scripts/Coin.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Coin : MonoBehaviour {
+	public CircleCollider2D coinEdge;
+	public UnityEngine.UI.Text coinText;
+	public GameObject coinObject;
+	public static int coinCount=0;
+	public AudioSource coinSoundFx;
+
+	// Use this for initialization
+	void Start () {
+		coinEdge= GetComponent<CircleCollider2D>();
+		coinEdge.enabled= true;
+		coinObject.SetActive(true);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (coinEdge.IsTouchingLayers()==true) {
+			coinSoundFx.Play ();
+			coinEdge.enabled= false;
+			coinObject.SetActive(false);
+			coinCount = coinCount + 1;
+
+	}
+		coinText.text = "Coins: " + coinCount;
+}
+}
---
+++ CIS487_2D/Assets/Scripts/RestartLevel.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class RestartLevel : MonoBehaviour {
+	public int sceneInt;
+	// Use this for initialization
+	void Start () {
+		sceneInt = SceneManager.GetActiveScene ().buildIndex;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Clicked () {
+		Time.timeScale = 1; // unpause
+		Gas.gasAmount = 100;//resetting static variables for level restart
+
+		SceneManager.LoadScene (sceneInt);
+}
+}
