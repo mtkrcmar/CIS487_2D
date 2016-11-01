6015bff22fa4341d47c61779864a19235fed0b75
mtkrcmar <michael.krcmarik@gmail.com>
2016-11-01 00:21:10 -0400

    Daily work 10.31
    - Added and designed a new level.
    - Turned said level into a prefab
    
    Issues:
    - Couldn't close upgrade panel so I tried to add a close button and cant
      get it fully working, maybe you can assist?
    - Didn't add as much new stuff as i'd like :)
    
    Happy Halloween!

---
+++ CIS487_2D/Assets/ClosePanel.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClosePanel : MonoBehaviour {
+
+    public GameObject menuPanel;
+
+	// Use this for initialization
+	void Start () {
+        menuPanel = GameObject.FindGameObjectWithTag("UpgradePanel");
+    }
+	
+	public void Close () {
+        Debug.Log(string.Format("The upgrade panel is: {0}", menuPanel.ToString()));
+        menuPanel.SetActive(false);
+	}
+}
